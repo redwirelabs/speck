a802ea9b3d56899b74f37bc550e51161e9aca142
Alex McLain <alex@alexmclain.com>
2023-07-27 22:35:53 -0700

    Update HTTP.GetDevice.V1 regex

--- protocol/http/devices/get.v1.ex
+++ protocol/http/devices/get.v1.ex
@@ -4,4 +4,4 @@ name "get_device"
 endpoint "/device/:uuid"
 verb "get"
 
-attribute :uuid, :string, format: ~r/\Ad{5}\-d{5}\-d{5}\-d{5}\-d{5}\z/
+attribute :uuid, :string, format: ~r/\A\d{5}\-\d{5}\-\d{5}\-\d{5}\-\d{5}\z/
