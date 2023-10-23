05701ae2cad91b6eaee5a7ef6b1ae0d819989c10
Alex McLain <alex@alexmclain.com>
2023-10-22 17:26:19 -0700

    Version 0.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Speck.MixProject do
   def project do
     [
       app: :speck,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env == :prod,
       compilers: Mix.compilers ++ [:speck],
