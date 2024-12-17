8afd2b1af1d41f26462f0ba809498e64aadf9a0e
abelino <abelino.romo@gmail.com>
2024-12-16 20:27:53 -0800

    Version 1.2.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Speck.MixProject do
   def project do
     [
       app: :speck,
-      version: "1.1.0",
+      version: "1.2.0",
       elixir: "~> 1.14",
       start_permanent: Mix.env == :prod,
       compilers: Mix.compilers ++ [:speck],
